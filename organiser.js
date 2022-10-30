ff88bb1aa398fc0b8cfca793fd5d41249d60d72f
unknown <dhaneshwarlingam@gmail.com>
2022-10-31 00:40:34 +0530

    stun server details

--- organiser.js
+++ organiser.js
@@ -25,19 +25,19 @@ let uid = Date.now().toString(36) + Math.random().toString(36).substring(2);
 const stunServers = {
     iceServers:[
         {
-            urls:[ 'stun.l.google.com:19302',
-                'stun1.l.google.com:19302',
-                'stun2.l.google.com:19302',
-                  'stun:stun3.l.google.com:19302',
+            urls:[ 'stun:stun.l.google.com:19302',
+                   'stun:stun1.l.google.com:19302',
+                   'stun:stun2.l.google.com:19302',
+                   'stun:stun3.l.google.com:19302',
                    'stun:stun4.l.google.com:19302',
-                   'stun.ideasip.com',
-                   'stun.rixtelecom.se',
-                   'stun.schlund.de',
-                   'stun.stunprotocol.org:3478',
-                   'stun.voiparound.com',
-                   'stun.voipbuster.com',
-                   'stun.voipstunt.com',
-                   'stun.voxgratia.org'
+                   'stun:stun.ideasip.com',
+                   'stun:stun:stun.rixtelecom.se',
+                   'stun:stun.schlund.de',
+                   'stun:stun.stunprotocol.org:3478',
+                   'stun:stun.voiparound.com',
+                   'stun:stun.voipbuster.com',
+                   'stun:stun.voipstunt.com',
+                   'stun:stun.voxgratia.org'
                 ]
         }
     ]
